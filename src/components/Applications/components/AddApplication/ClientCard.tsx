c8587891012f2cf7a63ad17b8025a7cc6dd72cf8
mirjahon <mirjahonoralov@gmail.com>
2023-05-24 19:13:31 +0500

    changed api

--- src/components/Applications/components/AddApplication/ClientCard.tsx
+++ src/components/Applications/components/AddApplication/ClientCard.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, CardBody, CardTop, InputWrapper } from "../Header/style";
 import CustomInput from "../../../Common/CustomInput";
 import CustomSelect from "../../../Common/CustomSelect";
 import { appsDataT } from "../../../../store/slices/applicationsSlice";
 import { useAppSelector } from "../../../../hooks/reduxHooks";
-import { SelectChangeEvent } from "@mui/material";
+import { SelectChangeEvent, Stack } from "@mui/material";
 import { dataType } from "../../../../store/slices/contactSlice";
 import { clientDataT } from ".";
+import AvatarWrapper from "../../../Common/AvatarWrapper";
+import { CameraAlt } from "@mui/icons-material";
 
 type T = {
   checkErrors: boolean;
@@ -26,6 +28,7 @@ const ClientCard: React.FC<T> = ({
   onChange,
 }) => {
   const { Data } = useAppSelector((state) => state.settings);
+  const [imageFile, setImageFile] = useState<File | null>(null);
 
   return (
     <Card>
@@ -117,6 +120,25 @@ const ClientCard: React.FC<T> = ({
             pl="10px"
           />
         </InputWrapper>
+        <Stack alignItems={"center"}>
+          <input
+            type="file"
+            id="avatar"
+            style={{ display: "none" }}
+            onChange={(e) => setImageFile(e.target.files![0])}
+            accept="image/*"
+          />
+
+          <label htmlFor="avatar">
+            {imageFile ? (
+              <AvatarWrapper size="64px" url={URL.createObjectURL(imageFile)} />
+            ) : clientData.image ? (
+              <AvatarWrapper size="64px" url={clientData.image as string} />
+            ) : (
+              <CameraAlt fontSize="large" style={{ cursor: "pointer" }} />
+            )}
+          </label>
+        </Stack>
       </CardBody>
     </Card>
   );
